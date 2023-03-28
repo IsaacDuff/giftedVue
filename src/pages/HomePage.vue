6309ee06efd183dfd8e72ba8c0a0d14c36f7a751
Isaac Duff <4mustangid@gmail.com>
2023-03-28 14:46:41 -0600

    first commit

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
+    <div class="home-card p-5 bg-white rounded elevation-3">
+      <img src="https://bcw.blob.core.windows.net/public/img/8600856373152463" alt="CodeWorks Logo"
+        class="rounded-circle">
+      <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
+        Vue 3 Starter
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+import { giftsService } from '../services/GiftsService.js'
+import { logger } from '../utils/Logger.js'
+
+export default {
+  setup() {
+
+    async function getGifts() {
+      try {
+        logger.log('getting gifts to screen')
+        // await giftsService.getGifts()
+      } catch (error) {
+        logger.log(error.message)
+      }
+    }
+
+    return {}
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.home {
+  display: grid;
+  height: 80vh;
+  place-content: center;
+  text-align: center;
+  user-select: none;
+
+  .home-card {
+    width: 50vw;
+
+    >img {
+      height: 200px;
+      max-width: 200px;
+      width: 100%;
+      object-fit: contain;
+      object-position: center;
+    }
+  }
+}
+</style>
